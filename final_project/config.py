93c4fee85d1cee181f971d90454047957a8519ab
sonyadamiyan <sonyadamiyan7@gmail.com>
2024-05-13 23:33:37 +0300

    file

--- final_project/config.py
+++ final_project/config.py
@@ -2,7 +2,7 @@ LANGUAGE = "ru-RU"
 SPEECHKIT_MODEL = "general"
 VOICE = "ermil"
 
-HOME_DIR = 'home/student/final_projectt/final_project'
+HOME_DIR = 'home/student/final_bot/final_project'
 LOGS = f'{HOME_DIR}/logs.txt'
 DB_FILE = f'{HOME_DIR}/messages.db'
 
